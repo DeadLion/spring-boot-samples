718e75ca4e8b9882d0f9b220c1de8d51377cc602
deadlion <zhongjianno1@qq.com>
2016-06-30 18:07:52 +0800

    add the simple project

---
+++ simple-applicaiton/src/main/java/Application.java
@@ -0,0 +1,9 @@
+/**
+ * Created by admin on 2016/6/30.
+ */
+@SpringBootApplication
+public class SimpleApplication {
+    public static void main(String[] args) {
+        SimpleApplication.run(Application.class, args);
+    }
+}
