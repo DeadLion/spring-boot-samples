6ba9d436b9e7d0e8ac4af9c159eb54b5b66b9f38
deadlion <zhongjianno1@qq.com>
2016-09-01 14:30:06 +0800

    clean

--- simple-applicaiton/src/main/java/Application.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by admin on 2016/6/30.
- */
-@SpringBootApplication
-public class SimpleApplication {
-    public static void main(String[] args) {
-        SimpleApplication.run(Application.class, args);
-    }
-}
