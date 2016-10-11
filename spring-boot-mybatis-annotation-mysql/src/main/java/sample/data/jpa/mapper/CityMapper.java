2b23a16fa99126e1ad4138a2f608a70157dcc615
deadlion <zhongjianno1@qq.com>
2016-10-11 15:27:40 +0800

    update

---
+++ spring-boot-mybatis-annotation-mysql/src/main/java/sample/data/jpa/mapper/CityMapper.java
@@ -0,0 +1,16 @@
+package sample.data.jpa.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import sample.data.jpa.domain.City;
+
+/**
+ * Created by admin on 2016/10/11.
+ */
+@Mapper
+public interface CityMapper {
+
+    @Select("select * from city where name =#{name}")
+    City findByName(@Param("name") String name);
+}
